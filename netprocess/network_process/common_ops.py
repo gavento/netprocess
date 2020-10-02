c1059ea044ead625997553549954f71eae2ffdc2
Tomas Gavenciak <gavento@gmail.com>
2020-10-02 02:23:41 +0200

    Add simple plotting helpers to ops.

--- netprocess/network_process/common_ops.py
+++ netprocess/network_process/common_ops.py
@@ -1,14 +1,22 @@
+from typing import List, Union
+
 import jax
 import jax.numpy as jnp
 
 from ..utils import PRNGKey, PytreeDict
-from .state import ProcessStateData
 from .operation import OperationBase
+from .state import ProcessState, ProcessStateData
 
 
 class CountNodeStatesOp(OperationBase):
-    def __init__(self, states: int, key: str = "state", dest: str = None):
-        self.states = states
+    def __init__(
+        self, states: Union[int, List[str]], key: str = "state", dest: str = None
+    ):
+        if isinstance(states, int):
+            self.state_names = [f"S{i}" for i in range(states)]
+        else:
+            self.state_names = states
+        self.states = len(self.state_names)
         self.key = key
         self.dest = dest if dest is not None else f"{self.key}_count"
 
@@ -23,10 +31,27 @@ class CountNodeStatesOp(OperationBase):
         )
         return {self.dest: counts}
 
+    def get_traces(self, state: ProcessState):
+        """
+        Return a dict `{trace_name: y_array}` for plotting.
+
+        Includes `"x": 0..s-1`.
+        """
+        data = state.all_records()[self.dest]
+        d = {s: data[:, i] for s, i in enumerate(self.state_names)}
+        d.update(x=jnp.arange(data.shape[0]))
+        return d
+
 
 class CountNodeTransitionsOp(OperationBase):
-    def __init__(self, states: int, key: str = "state", dest: str = None):
-        self.states = states
+    def __init__(
+        self, states: Union[int, List[str]], key: str = "state", dest: str = None
+    ):
+        if isinstance(states, int):
+            self.state_names = [f"S{i}" for i in range(states)]
+        else:
+            self.state_names = states
+        self.states = len(self.state_names)
         self.key = key
         self.dest = dest if dest is not None else f"{self.key}_transitions"
 
@@ -43,3 +68,25 @@ class CountNodeTransitionsOp(OperationBase):
         counts = jnp.sum(jax.nn.one_hot(transitions, self.states * self.states), axis=0)
         counts_from_to = jnp.reshape(counts, (self.states, self.states))
         return {self.dest: counts_from_to}
+
+    def get_traces(
+        self, state: ProcessState, diagonal=False, zeros=False, fraction=False
+    ):
+        """
+        Return a dict `{trace_name: y_array}` for plotting.
+
+        Includes `"x": 0..s-1`.
+        Optionally contains diagonal traces (state to itself), transitions that never
+        happened, and/or contains fraction from source state rather than counts.
+        """
+        data = state.all_records()[self.dest]
+        if fraction:
+            data = data / state.n
+        d = {}
+        for s0, i0 in enumerate(self.state_names):
+            for s1, i1 in enumerate(self.state_names):
+                r = data[:, i0, i1]
+                if (diagonal or i0 != i1) and (zeros or jnp.sum(r) > 0):
+                    d[f"{s0} -> {s1}"] = r
+        d.update(x=jnp.arange(data.shape[0]))
+        return d
